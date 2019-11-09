e59ceb18f792fd8c19bb8cd1dfb060e21f447677
tjbrunson <tjbrunson@gmail.com>
2019-11-09 11:42:52 -0800

    moved errors to its own blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,10 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
 
 if not app.debug:
     if not os.path.exists('logs'):
