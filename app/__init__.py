f6bd599f60c9dd87f920d46b90232cc2f3d699c8
tjbrunson <tjbrunson@gmail.com>
2019-11-09 13:12:49 -0800

    moved all authorization to new package

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,8 @@ bootstrap = Bootstrap(app)
 
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 from app import routes, models
 
