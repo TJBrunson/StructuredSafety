294d82730a3695ae715edf28c25522fa5c88e19b
Tim Brunson <tjbrunson@gmail.com>
2019-11-17 23:10:10 -0700

    reset migrations and db

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    companies = db.relationship('Company', backref='employee', lazy='dynamic')
+    company = db.Column(db.Integer, db.ForeignKey('company.id'))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -29,6 +29,7 @@ class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     company_name = db.Column(db.String(64), index=True, unique=True)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    employees = db.relationship('User', backref='employee', lazy='dynamic')
 
     def __repr__(self):
         return '<Company {}>'.format(self.company_name)
