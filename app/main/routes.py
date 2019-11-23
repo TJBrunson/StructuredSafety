50124de696b34fd2e710eef30ab750ca922980eb
Tim Brunson <tjbrunson@gmail.com>
2019-11-23 12:58:12 -0800

    added company link in navbar when a company exists for a user

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,7 +14,8 @@ def index():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    company = Company.query.filter_by(owner_id = user.id).first_or_404()
+    return render_template('user.html', user=user, company=company)
 
 @bp.route('/demo')
 def demo():
@@ -23,5 +24,5 @@ def demo():
 @bp.route('/company/<company_name>')
 def company(company_name):
     company = Company.query.filter_by(company_name=company_name).first_or_404()
-    user = User.query.filter_by(id=company.owner_id).first_or_404()
+    user = User.query.filter_by(username=current_user.username).first_or_404()
     return render_template('company.html', company=company, user=user)
